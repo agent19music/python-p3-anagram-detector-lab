e7e32dd9c6ee0284b626c9ce41610d46b636fa3a
agent19music <seanmotanya@gmail.com>
2023-12-09 10:29:16 +0300

    Done with assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python3
 # your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word_characters = sorted([letter for letter in word])
+    def match(self, word_list):
+        match_word_list = []
+
+        for word in word_list:
+            if sorted([letter for letter in word]) == self.word_characters:
+                match_word_list.append(word)
+
+        return match_word_list
+
+one_instance = Anagram('listen')
+print(one_instance.match(['enlist', 'google', 'inlets', 'banana']))  # Output: ['enlist', 'inlets']
+
